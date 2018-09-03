660b98a09ebf1253b58116ab8251fc570642e5d5
Federico <fgomezl@unal.edu.co>
2018-09-02 20:23:17 -0500

    added date picker on MainScreen

--- app/src/main/java/fedejandro/encuestadigital/MainScreen.java
+++ app/src/main/java/fedejandro/encuestadigital/MainScreen.java
@@ -1,26 +1,30 @@
 package fedejandro.encuestadigital;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.Calendar;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
-public class MainScreen extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity implements View.OnClickListener {
 
     EditText fechaText;
     EditText encuestadorText;
     EditText consecutivoText;
-    Button nextButton;
+    Button nextButton, fechaButton;
+    private int day, month, year;
 
     private String filename = "datos.txt";
     private String filepath = "EncuestaDatos";
@@ -36,8 +40,10 @@ public class MainScreen extends AppCompatActivity {
         encuestadorText = (EditText) findViewById(R.id.encuestadorText);
         consecutivoText = (EditText) findViewById(R.id.consecutivoText);
 
-        nextButton =
-                (Button) findViewById(R.id.nextButton);
+        fechaButton = (Button) findViewById(R.id.fechaButton);
+        fechaButton.setOnClickListener(this);
+
+        nextButton =  (Button) findViewById(R.id.nextButton);
         nextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -84,5 +90,25 @@ public class MainScreen extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==fechaButton){
+            final Calendar myCalendar = Calendar.getInstance();
+            day=myCalendar.get(Calendar.DAY_OF_MONTH);
+            month=myCalendar.get(Calendar.MONTH);
+            year=myCalendar.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    fechaText.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                }
+            }
+            , year, month, day);
+            datePickerDialog.show();
+        }
+
     }
 }
